e6829995b2e3977304273f1094860b7ca24b4981
goDeni <den4ikept11@gmail.com>
2023-08-26 11:40:08 +0500

    fix

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,9 +1,4 @@
-use std::path::Path;
-
-use sec_store::repository::RecordsRepository;
-
 extern crate sec_store;
-
 
 fn main() {
     println!("Hello, world!");
--- bot/src/users.rs
+++ bot/src/users.rs
@@ -1,42 +1,30 @@
-use std::{
-    fmt::format,
-    path::{Path, PathBuf},
+use std::path::PathBuf;
+
+use sec_store::{
+    cipher::EncryptionKey,
+    repository::{OpenResult, RecordsRepository},
 };
 
-use sec_store::{cipher::EncryptionKey, repository::RecordsRepository};
-
 type UserId = &'static str;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct RepositoryDoesntExist;
-pub type GetRepoResult<T> = std::result::Result<T, RepositoryDoesntExist>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct RepositoryAlreadyExist;
 pub type InitRepoResult<T> = std::result::Result<T, RepositoryAlreadyExist>;
 
-struct RepositoriesFabric(PathBuf);
-impl RepositoriesFabric {
+struct RepositoriesFactory(PathBuf);
+impl RepositoriesFactory {
     fn get_repository_path(&self, user_id: UserId) -> PathBuf {
         self.0.join(format!("rep_{}", user_id))
     }
-    pub fn user_has_repository(&self, user_id: &UserId) -> bool {
+    pub fn user_has_repository(&self, user_id: UserId) -> bool {
         self.get_repository_path(user_id).exists()
     }
     pub fn get_user_repository(
         &self,
         user_id: UserId,
         passwd: EncryptionKey,
-    ) -> GetRepoResult<RecordsRepository> {
-        match self.user_has_repository(&user_id) {
-            true => {
-                let path = self.get_repository_path(&user_id);
-                let res = RecordsRepository::open(path, passwd);
-
-                Ok(res.unwrap())
-            }
-            false => Err(RepositoryDoesntExist),
-        }
+    ) -> OpenResult<RecordsRepository> {
+        RecordsRepository::open(self.get_repository_path(&user_id), passwd)
     }
     pub fn initialize_user_repository(
         &self,
@@ -45,12 +33,70 @@ impl RepositoriesFabric {
     ) -> InitRepoResult<RecordsRepository> {
         match self.user_has_repository(&user_id) {
             true => Err(RepositoryAlreadyExist),
-            false => {
-                let path = self.get_repository_path(&user_id);
-                let repo = RecordsRepository::new(path, passwd);
-
-                Ok(repo)
-            }
+            false => Ok(RecordsRepository::new(
+                self.get_repository_path(user_id),
+                passwd,
+            )),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sec_store::repository::RepositoryOpenError;
+    use tempdir::TempDir;
+
+    use super::RepositoriesFactory;
+
+    #[test]
+    fn test_repo_not_exist() {
+        let tmp_dir = TempDir::new("tests_").unwrap();
+
+        let user_id = "user_id";
+        let passwd = "123";
+
+        let factory = RepositoriesFactory(tmp_dir.into_path());
+
+        assert!(!factory.user_has_repository(user_id));
+        let result = factory.get_user_repository(user_id, passwd).unwrap_err();
+
+        assert_eq!(result, RepositoryOpenError::FileAccessError);
+    }
+
+    #[test]
+    fn test_repo_initialization() {
+        let tmp_dir = TempDir::new("tests_").unwrap();
+
+        let user_id = "user_id";
+        let passwd = "123";
+
+        let factory = RepositoriesFactory(tmp_dir.into_path());
+
+        let repo = factory.initialize_user_repository(user_id, passwd).unwrap();
+        repo.save().unwrap();
+
+        assert!(factory.user_has_repository(user_id));
+
+        let result = factory.get_user_repository(user_id, passwd);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_repo_open_with_wrong_password() {
+        let tmp_dir = TempDir::new("tests_").unwrap();
+
+        let user_id = "user_id";
+        let passwd = "123";
+
+        let factory = RepositoriesFactory(tmp_dir.into_path());
+
+        factory
+            .initialize_user_repository(user_id, passwd)
+            .unwrap()
+            .save()
+            .unwrap();
+        let result = factory.get_user_repository(user_id, "312").unwrap_err();
+
+        assert_eq!(result, RepositoryOpenError::WrongPassword);
     }
 }

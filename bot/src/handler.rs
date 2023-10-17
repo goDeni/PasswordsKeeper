15742b29b5da3fa05b6198c176bcb1a4f2ec7911
Denis <den4ikept11@gmail.com>
2023-10-17 12:45:06 +0500

    ttl feature (#28)

--- bot/src/handler.rs
+++ bot/src/handler.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashMap, error::Error, future::IntoFuture, marker::PhantomData};
+use std::{
+    collections::HashMap,
+    error::Error,
+    future::IntoFuture,
+    marker::PhantomData,
+    time::{Duration, SystemTime},
+};
 
 use sec_store::repository::RecordsRepository;
 use teloxide::{
@@ -17,9 +23,12 @@ use teloxide::{
     },
     Bot,
 };
-use tokio::{sync::RwLock, task::JoinSet};
+use tokio::{sync::RwLock, task::JoinSet, time::sleep};
 
-use crate::{dialogues::hello::HelloDialogue, user_repo_factory::RepositoriesFactory};
+use crate::{
+    dialogues::hello::HelloDialogue, dialogues_controller::CtxResult,
+    user_repo_factory::RepositoriesFactory,
+};
 use crate::{
     dialogues_controller::{self, DialInteraction, DialogueController},
     stated_dialogues::{MessageFormat, MessageId},
@@ -212,6 +221,109 @@ async fn handle_interaction<F: RepositoriesFactory<R>, R: RecordsRepository>(
         context.write().await.dial_ctxs.insert(*user_id, controller);
     }
     Ok(())
+}
+
+const DIALOG_CONTROLLER_TTL_SECONDS: u64 = 300;
+pub async fn track_dialog_ttl<F: RepositoriesFactory<R>, R: RecordsRepository>(
+    bot_context: Arc<RwLock<BotContext<F, R>>>,
+    bot: Bot,
+) {
+    loop {
+        let current_time = SystemTime::now();
+
+        let result = bot_context
+            .read()
+            .await
+            .dial_ctxs
+            .iter()
+            .map(|(user_id, controller)| {
+                (
+                    *user_id,
+                    current_time
+                        .duration_since(*controller.get_last_interaction_time())
+                        .unwrap(),
+                )
+            })
+            .collect::<Vec<(UserId, Duration)>>();
+
+        let sleep_time = result
+            .iter()
+            .filter_map(|(_, duration)| {
+                duration
+                    .as_secs()
+                    .le(&DIALOG_CONTROLLER_TTL_SECONDS)
+                    .then(|| {
+                        Some(Duration::from_secs(
+                            DIALOG_CONTROLLER_TTL_SECONDS - duration.as_secs(),
+                        ))
+                    })
+                    .unwrap_or(None)
+            })
+            .max()
+            .unwrap_or_else(|| Duration::from_secs(DIALOG_CONTROLLER_TTL_SECONDS));
+
+        let keys_to_remove = result
+            .iter()
+            .filter_map(|(user_id, duration)| {
+                if duration.as_secs().ge(&DIALOG_CONTROLLER_TTL_SECONDS) {
+                    Some(user_id)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&UserId>>();
+
+        if !keys_to_remove.is_empty() {
+            log::debug!("[ttl controller] Remove {} dialogs", keys_to_remove.len());
+            let mut context_wlock = bot_context.write().await;
+
+            let result = keys_to_remove
+                .into_iter()
+                .filter_map(|user_id| {
+                    context_wlock
+                        .dial_ctxs
+                        .remove(user_id)
+                        .map(|controller| (*user_id, controller))
+                })
+                .filter_map(|(user_id, controller)| match controller.shutdown() {
+                    Ok(result) => Some((user_id, result)),
+                    Err(err) => {
+                        log::error!("[ttl controller] Failed dialog shutdown {}", err);
+                        None
+                    }
+                })
+                .collect::<Vec<(UserId, Vec<CtxResult>)>>();
+            drop(context_wlock);
+
+            for (user_id, ctx_results) in result {
+                if let Err(err) = process_ctx_results(user_id, ctx_results, &bot).await {
+                    log::error!(
+                        "[ttl controller] Failed results processing for {}: {}",
+                        user_id,
+                        err
+                    );
+                } else if let Err(err) = bot
+                    .send_message(
+                        user_id,
+                        format!(
+                            "Диалог закрыт потому что не использовался {} секунд 🙈\nВведите /start чтобы инициировать новый диалог",
+                            DIALOG_CONTROLLER_TTL_SECONDS
+                        ),
+                    )
+                    .await
+                {
+                    log::error!(
+                        "[ttl controller] Failed send message for user {}: {}",
+                        user_id,
+                        err
+                    )
+                }
+            }
+        }
+
+        log::debug!("[ttl controller] Sleep {} seconds", sleep_time.as_secs());
+        sleep(sleep_time).await;
+    }
 }
 
 fn create_dial_controller<F: RepositoriesFactory<R>, R: RecordsRepository>(
